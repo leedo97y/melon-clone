b6fa86c672b1f49232a75714b58e70f37421d704
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-04-24 17:54:46 +0900

    fix webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
 
@@ -13,10 +13,9 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
     }),
-    new HtmlWebpackPlugin({
-      template: "./src/views/home.pug",
-      filename: "index.html",
-    }),
+    // new HtmlWebpackPlugin({
+    //   filename: "base.pug",
+    // }),
     new webpack.EnvironmentPlugin({
       API_KEY: "9ef77bf80f5f45115d9ad6df6c2de8ec",
       JWT_SECRET_KEY: "tomato",
@@ -46,29 +45,17 @@ module.exports = {
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/,
-        include: [path.resolve(__dirname, "src/client/images")],
         loader: "file-loader",
         options: {
-          name: "[name].[ext]",
-          outputPath: "images/",
+          name: "images/[name].[ext]",
         },
       },
       {
         test: /\.mp3$/,
-        include: [path.resolve(__dirname, "src/client/audios")],
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "audios/",
-            },
-          },
-        ],
-      },
-      {
-        test: /\.pug$/,
-        use: ["pug-loader"],
+        loader: "file-loader",
+        options: {
+          name: "audios/[name].[ext]",
+        },
       },
     ],
   },
